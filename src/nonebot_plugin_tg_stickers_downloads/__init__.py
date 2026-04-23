3a68fd24c26b3632f2bb2b44472d5b7bc0d1ac30
lyqgzbl <122811297+lyqgzbl@users.noreply.github.com>
2026-04-23 23:13:57 +0800

    🔖 v0.2.2

--- src/nonebot_plugin_tg_stickers_downloads/__init__.py
+++ src/nonebot_plugin_tg_stickers_downloads/__init__.py
@@ -39,7 +39,7 @@ __plugin_meta__ = PluginMetadata(
     supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
     extra={
         "author": "lyqgzbl <admin@lyqgzbl.com",
-        "version": "0.2.1",
+        "version": "0.2.2",
     },
 )
 
