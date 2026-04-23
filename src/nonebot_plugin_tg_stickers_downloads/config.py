a597fc8e8839d720c4747a178287647abcf8ca7b
lyqgzbl <122811297+lyqgzbl@users.noreply.github.com>
2026-04-23 22:52:54 +0800

    ✨ 添加配置项以支持插件功能，优化插件配置获取逻辑

--- src/nonebot_plugin_tg_stickers_downloads/config.py
+++ src/nonebot_plugin_tg_stickers_downloads/config.py
@@ -1,3 +1,4 @@
+from nonebot import get_plugin_config
 from pydantic import BaseModel
 
 
@@ -7,3 +8,11 @@ class Config(BaseModel):
     tgsd_ffmpeg_path: str | None = None
     tgsd_gifsicle_path: str | None = None
     tgsd_imagemagick_path: str | None = None
+    tgsd_cache_expire_seconds: int = 24 * 3600
+    tgsd_download_concurrency: int = 5
+    tgsd_convert_concurrency: int = 2
+    tgsd_skip_conversion: bool = False
+    tgsd_subprocess_timeout: int = 120
+
+
+plugin_config = get_plugin_config(Config)
